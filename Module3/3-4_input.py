9d3e3b19f3dad58690caf5354a9ed129568a1668
leo53021313 <leo53021313@gmail.com>
2026-03-29 16:34:18 +0800

    新上課代碼2026/03/29

--- Module3/3-4_input.py
+++ Module3/3-4_input.py
@@ -1,6 +1,12 @@
 # input() 函數 讓使用者在終端機輸入資料
 
 # 取得使用者輸入的資料
+data = input("請輸入數字: ")
+print(type(data))
 
+data = int(data)
+print(type(data))
+
+print(data + 100)
 
 # 將使用者輸入強制轉型成 int
--- Module3/practice.py
+++ Module3/practice.py
@@ -4,15 +4,23 @@
 # 數字太大 要提示「請輸入更小的數字」
 # 數字太小 要提示「請輸入更大的數字」
 # 使用者猜對要回傳「恭喜中獎」
+import random
 
+user_input = -1
+random_int = random.randint(1, 100)
+# print("Rand: " + str(random_int))
 
+while True:
+    user_input = int(input("請輸入 1 到 100 之間的整數: "))
+    if user_input > 100 or user_input < 1:
+        print("請填寫 1 到 100 的整數!")
+        continue
 
-
-
-
-
-
-
-
-
-
+    # print("你填入了: " + str(user_input))
+    if user_input == random_int:
+        print("你猜對了!")
+        break
+    elif user_input > random_int:
+        print("數字太大了，繼續猜小一點!")
+    elif user_input < random_int:
+        print("數字太小了，繼續猜大一點!")
